3f98318afd6358337c183fd2873bc8b9fb174444
Ankan Bhattacharjee <113756091+Orsted10@users.noreply.github.com>
2025-08-27 09:25:50 +0530

    Delete 1. Print name, course, and university.c

--- 1. Print name, course, and university.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("Name: Ankan Bhattacharjee\n");
-    printf("Course: Computer Science Engineering\n");
-    printf("University: Chandigarh University, Uttar Pradesh\n");
-    return 0;
-}
